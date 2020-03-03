68969f0061b2185fc58f36af855ce33706d117ff
Ari6 <24240826+Ari6@users.noreply.github.com>
2020-03-02 20:52:15 -0800

    start making models

--- flashcard/flash/models.py
+++ flashcard/flash/models.py
@@ -1,17 +1,34 @@
 from django.db import models
 from django.util import timezone
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+class CustomUser(AbstractUser):
+    pass
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=20)
+    create = models.DateTimeField()
+    update = models.DateTimeField()
+    visible = models.IntegerField()
 
 class Question(models.Model):
     type = models.IntegerField()
     sidea = models.TextField()
     sideb = models.TextField()
-    user = models.ForeignKey(on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     group = models.ForeignKey(Group)
     create = models.DateTimeField()
     update = models.DateTimeField()
 
-class Group(models.Model):
-    g_name = models.CharField(max_length=20)
-    create = models.DateTimeField()
-    update = models.DateTimeField()
-    visible = models.IntegerField()
+class Log(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    questioin = models.ForeignKey(Question, on_delete=models.CASCADE)
+    last_time_correct = models.BooleanField(default=False)
+    continuous_correct = models.IntegerField()
+    correct_count = models.IntegerField()
+    show_count = models.IntegerField()
+
+class Config(models.Model):
+    threshhold = models.IntegerField()
+
